224635dc0b1bc65453cd1c415a6ad2e162b435d6
joaquinsanchezgomez <89624741+joaquinsanchezgomez@users.noreply.github.com>
2023-04-05 22:03:17 -0400

    Add files via upload

--- midterm.R
+++ midterm.R
@@ -1,15 +1,12 @@
 #Joaquin Sanchez Gomez & Holli Tai
 #midterm
 
-install.packages("tidyverse")
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("flexdashboard")
-install.packages("plotly")
-install.packages("leaflet")
-install.packages("dygraphs")
-install.packages("metricsgraphics")
-install.packages("readr")
+library(tidyverse)
+library(flexdashboard)
+library(plotly)
+library(leaflet)
+library(dygraphs)
+library(metricsgraphics)
 
 
 getwd
@@ -54,12 +51,133 @@ View(DUI_Crashes)
 Distracted_Driver_Crashes <- read.csv("Distracted_Driver_Crashes.csv")
 View(Distracted_Driver_Crashes)
 
-colnames(DUI_Crashes)
+accidents_CT <- bind_rows(Distracted_Driver_Crashes,DUI_Crashes,Fixed_Object_Crashes,Intersection_Crashes,Motorcycle_Crashes,Non_Motorist_Crashes,Older_Driver_Crashes,Pedestrian_Crashes,Unlicensed_Driver_Crashes,Work_Zone_Crashes,Wrong_Way_Crashes,Young_Driver_Crashes)
+
+accidents_CT <- accidents_CT %>% distinct()
+
+accidents_CT %>% group_by(CrashID) %>% count() %>% filter(n >1)
+
+accidents_CTex <- Fixed_Object_Crashes %>% mutate(Fixed_Object=1) %>% select(CrashID, Fixed_Object) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CT, by=c("CrashID"))
 
 
-accident_list <- list(Distracted_Driver_Crashes,DUI_Crashes,Fixed_Object_Crashes,Intersection_Crashes,Motorcycle_Crashes,Non_Motorist_Crashes,Older_Driver_Crashes,Pedestrian_Crashes,Unlicensed_Driver_Crashes,Work_Zone_Crashes,Wrong_Way_Crashes,Young_Driver_Crashes)
-accident_list
+accidents_CTex <- Distracted_Driver_Crashes %>% mutate(Distracted=1) %>% select(CrashID, Distracted) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
 
-accident_data_frame <- accident_list %>% reduce(full_join, by='CrashID')
-accident_data_frame
+accidents_CTex <- DUI_Crashes %>% mutate(DUI=1) %>% select(CrashID, DUI) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Intersection_Crashes %>% mutate(Intersection=1) %>% select(CrashID, Intersection) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Motorcycle_Crashes %>% mutate(Moto=1) %>% select(CrashID, Moto) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Non_Motorist_Crashes %>% mutate(Non_motorist=1) %>% select(CrashID, Non_motorist) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Older_Driver_Crashes %>% mutate(Old_people=1) %>% select(CrashID, Old_people) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Pedestrian_Crashes %>% mutate(Pedestrian=1) %>% select(CrashID, Pedestrian) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Unlicensed_Driver_Crashes %>% mutate(Unlicensed=1) %>% select(CrashID, Unlicensed) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Work_Zone_Crashes %>% mutate(Work_zone=1) %>% select(CrashID, Work_zone) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Wrong_Way_Crashes %>% mutate(wrong_way=1) %>% select(CrashID, wrong_way) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+accidents_CTex <- Young_Driver_Crashes %>% mutate(young_driver=1) %>% select(CrashID, young_driver) %>% distinct(CrashID, .keep_all = TRUE) %>%
+  right_join(., accidents_CTex, by=c("CrashID"))
+
+
+accidents_CTex <- accidents_CTex %>% replace(is.na(.),0)
+
+
+stamford <- accidents_CTex[accidents_CTex$CrashTownName == "Stamford", ]
+stamford
+
+
+### Drivers' age graph
+
+sum(stamford$young_driver == 1)
+
+sum(stamford$Old_people == 1)
+
+age_people <- data_frame(group=c("Young",
+                                "Old drivers" ),
+                         value= c(8722,5492))
+
+
+
+library(ggplot2)
+
+ggplot(age_people, aes(x = group, y = value, fill = group)) +
+  geom_bar(stat = "identity", width = 0.5) +
+  labs(title = "Accidents by Age Group", x = "Group", y = "Value")
+
+
+
+
+
+
+
+accidents_per_year <-stamford %>%group_by(CrashDateYear) %>% count() %>% arrange(desc(n))
+
+colnames(accidents_per_year)[2]='NumberofCrashes'
+colnames(accidents_per_year)[1]='Year'
+
+
+
+
+
+
+###  
+
+p <- ggplot(data=accidents_per_year,aes(x=Year,y=NumberofCrashes, fill=Year)) +
+  geom_bar(stat='identity')
+
+
+Crashes_Per_Year<- p + ggtitle("Stamford, CT: Accidents per Year") +
+  xlab("Year") + ylab("Crash Count")
+
+ggplotly(Crashes_Per_Year)
+
+
+
+
+###  
+
+most_day <- stamford %>%group_by(DayofWeek) %>% count %>% arrange(desc(n))
+
+
+colnames(most_day)[1]='DayofWeek'
+colnames(most_day)[2]='Crashes'
+
+day <- ggplot(data=most_day,aes(x=DayofWeek,y=Crashes, fill=DayofWeek)) +
+  geom_bar(stat='identity')
+
+
+days_most_crashes <-day + ggtitle("Crashes by Day of Week") +
+  xlab("Day of Week") + ylab ("Crash Count")
+
+
+ggplotly(days_most_crashes)
+
+###  
+Week_Hour <-stamford %>%group_by(DayofWeek,CrashTimeHour) %>% count %>% arrange(desc(n))
+
+colnames(Week_Hour)[1]='Day of Week'
+colnames(Week_Hour)[2]='Hour'
+colnames(Week_Hour)[3]='Crashes'
+datatable(
+  head(Week_Hour),
+  caption = 'Quantity of Crashes by Day and Hour')
+
+
+
 
